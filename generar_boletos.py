9d3393d3e14b55100219fa9ad68ad1fd61eb5959
emena16 <eduardo.cifer@gmail.com>
2019-11-07 07:59:51 -0600

    version v1 funcional

--- generar_boletos.py
+++ generar_boletos.py
@@ -1,7 +1,9 @@
 from fpdf import FPDF
 
 import datetime
+#Obtenemos la fecha
 x = datetime.datetime.now()
+
 #Creamos el objeto pdf que es quien se encargara de dibujar el PDF
 pdf = FPDF()
 folio_inicial = int(input("Ingresa folio inicial: "))
@@ -15,14 +17,14 @@ if paginas < 1:
 pdf.set_font('Arial', 'B', 16)
 i= k = 0
 
-fecha_x = [60,160,180]
-fecha_y = [15,30,100,140]
-pos_x = [[15,30,45],[15,30,45],[15,30,45],[15,30,45]]
-pos_y = [10,50,130,180]
+fecha_x = [32,83,135,187]
+fecha_y = [78,173,266.5]
+pos_x = [[20,70,123,175],[20,70,123,175],[20,70,123,175],[20,70,123,175]]
+pos_y = [5,100,193]
 #iteramos segun las filas solicitadas
 while i != paginas:
 	pdf.add_page()
-	pdf.image('fondo.png',0,0, 216, 279)
+	pdf.image('fondo.jpg',0,-7, 212, 290)
 	#son las posiciones en las que nos vamos a mover para poner los numeros
 	for k in range(len(pos_y)):
 		pdf.set_font('Arial', 'B', 16)
@@ -42,5 +44,5 @@ while i != paginas:
 file = open('salida.txt','w+')
 file.write('$$$ '+x.strftime("%d"+"/"+"%m"+"/"+"%Y")+' $$$ \n')
 file.write('Se han generado '+repr(paginas)+' con folio de: '+repr(folio_inicial)+' a '+repr(folio-1)+' \n')
-
-pdf.output('tuto1.pdf', 'F')
+file.close()
+pdf.output('boleto_estacionmamiento.pdf', 'F')
